7356c62a863055c46c884f31be9702b530cd166a
mengaoran <499491857@qq.com>
2018-09-06 08:50:59 +0800

    first commit

---
+++ src/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * 描述:
+ * 测试GIT专用
+ *
+ * @author a4994
+ * @create 2018-09-06 8:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+    }
+}
